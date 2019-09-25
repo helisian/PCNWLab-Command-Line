2e2d9b6cc8b74ad192e60cb3938ad80c24c3cfb9
Nilber Remon <nilberremon@gmail.com>
2019-09-24 22:50:56 -0400

    Problem Set 1 Complete

--- pset1.js
+++ pset1.js
@@ -3,7 +3,7 @@ PROBLEM 1:
 
 Follow the following steps ONLY using your command line:
 
-1. Navigate to your Desktop directory.
+1. Navigate to your Desktop directory. 
 2. Create a directory called my_first_direc.
 3. Enter the my_first_direc directory.
 4. Create a file called first.js.
@@ -16,8 +16,24 @@ Follow the following steps ONLY using your command line:
 11. Go up to the Desktop directory.
 12. Delete the test folder.
 
-******************/
+/Users/zone126/Desktop
+Zones-MacBook-Air:Desktop zone126$ mkdir my_first_direc
+Zones-MacBook-Air:Desktop zone126$ cd my_first_direc
+Zones-MacBook-Air:my_first_direc zone126$ touch first.js
+Zones-MacBook-Air:my_first_direc zone126$ pwd
+/Users/zone126/Desktop/my_first_direc
+Zones-MacBook-Air:my_first_direc zone126$ mkdir assignments
+Zones-MacBook-Air:my_first_direc zone126$ pwd
+/Users/zone126/Desktop/my_first_direc
+Zones-MacBook-Air:my_first_direc zone126$ mv first.js assignments
+Zones-MacBook-Air:my_first_direc zone126$ cd assignments
+Zones-MacBook-Air:assignments zone126$ rm first.js
+Zones-MacBook-Air:assignments zone126$ cd ..
+Zones-MacBook-Air:my_first_direc zone126$ rm -r assignments
+Zones-MacBook-Air:my_first_direc zone126$ cd ..
+Zones-MacBook-Air:Desktop zone126$ rm -r my_first_direc
 
+******************/
 
 
 /******************
@@ -34,6 +50,11 @@ Follow the following steps ONLY using your command line:
 8. Add the following in a new line: hellohellohello
 9. Exit without saving
 
+
+Zones-MacBook-Air:~ zone126$ cd Documents
+Zones-MacBook-Air:Documents zone126$ touch foo.js
+Zones-MacBook-Air:Documents zone126$ code foo.js
+Zones-MacBook-Air:Documents zone126$ code foo.js
 ******************/
 
 
